13f9627eb8842071d0cc7f4ba942d1e79a6783e4
biharihu <53407198+biharihu@users.noreply.github.com>
2019-12-14 03:21:44 +0530

    till lecture 206

---
+++ src/context/AuthContext.js
@@ -0,0 +1,54 @@
+import { AsyncStorage } from 'react-native';
+import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
+import { navigate } from '../navigationRef';
+
+const authReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_error':
+      return { ...state, errorMessage: action.payload };
+    case 'signup':
+      return { errorMessage: '', token: action.payload };
+    default:
+      return state;
+  }
+};
+
+const signup = dispatch => async ({ email, password }) => {
+  // make api request to sign up with that email and password
+  // if we sign up, modify our state, and say that we are authenticated
+  // if signing up fails, we probably need to reflect an error message
+  // somewhere
+  try {
+    const response = await trackerApi.post('/signup', { email, password });
+    await AsyncStorage.setItem('token', response.data.token);
+    dispatch({ type: 'signup', payload: response.data.token });
+
+    navigate('TrackList');
+  } catch (err) {
+    dispatch({
+      type: 'add_error',
+      payload: 'Something went wrong with sign up'
+    });
+  }
+};
+
+const signin = dispatch => {
+  return ({ email, password }) => {
+    // try to signin
+    // Handle success by updating state
+    // Handle failure by showing error message (somehow)
+  };
+};
+
+const signout = dispatch => {
+  return () => {
+    // somehow sign out!!!
+  };
+};
+
+export const { Provider, Context } = createDataContext(
+  authReducer,
+  { signin, signout, signup },
+  { token: null, errorMessage: '' }
+);
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,27 +1,74 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useState, useContext } from 'react';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
+import Spacer from '../components/Spacer';
+import { Context as AuthContext } from '../context/AuthContext';
 
 const SignupScreen = ({ navigation }) => {
+  const { state, signup } = useContext(AuthContext);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  console.log(state);
   return (
     <View style={styles.container}>
-      <Text>SignupScreen</Text>
-      <Button
-        title="Go to Signin"
-        onPress={() => navigation.navigate('Signin')}
+      <Spacer>
+        <Text h3>Sign Up for Tracker</Text>
+      </Spacer>
+      <Input
+        label="Email"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+        autoCorrect={false}
       />
-      <Button
-        title="Go to main flow"
-        onPress={() => navigation.navigate('mainFlow')}
+      <Spacer />
+      <Input
+        secureTextEntry
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+        autoCapitalize="none"
+        autoCorrect={false}
       />
+      {state.errorMessage ? (
+        <Text style={styles.errorMessage}>{state.errorMessage}</Text>
+      ) : null}
+      <Spacer>
+        <Button title="Sign Up" onPress={() => signup({ email, password })} />
+      </Spacer>
+      <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
+        <Text style={styles.signinText}>
+          Already have an account? Sign in instead
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
+
+SignupScreen.navigationOptions = () => {
+  return {
+    header: null
+  };
+};
+
 export default SignupScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    // alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 200
+  },
+  errorMessage: {
+    fontSize: 16,
+    alignSelf: 'center',
+    color: 'red',
+    marginTop: 15
+  },
+  signinText: {
+    color: 'blue',
+    fontSize: 16,
+    marginLeft: 15
   }
 });
